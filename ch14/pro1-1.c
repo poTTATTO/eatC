7967f826876a779ab7efd4889ee91d3a6cf2dc94
babamba <wuzulk158@gmail.com>
2026-01-03 01:03:14 +0900

    array -> struct library

--- ch14/pro1-1.c
+++ ch14/pro1-1.c
@@ -14,14 +14,13 @@ struct Book{
     int borrowed;
 };
 
-bool check_info(char* select, char* pinput, int size, int struct_size);
+bool check_info(struct Book* ptr, char* pinput, int size, int ch);
 void safety_input(char* p);
 int safety_input_num();
-void return_book(char** ptitle, char** pauthor, char** ppublisher, int* pborrow_possible);
-void borrow_book(char** ptitle, char** pauthor, char** ppublisher, int* pborrow_possible);
+void return_book(struct Book* ptr, int* pcount, int struct_size);
+void borrow_book(struct Book* ptr, int* pcount,int struct_size);
 void view_books(struct Book* ptr, int count);
-void add_book(char** ptitle, char** pauthor, char** ppublisher, int* plen_books, 
-    char (*pttmp)[MAX_OF_CONTENT], char (*patmp)[MAX_OF_CONTENT], char (*pptmp)[MAX_OF_CONTENT], int* pborrow_possible);
+void add_book(struct Book* ptr, int* pcount);
 void find(struct Book* ptr, int count, int struct_size);
 // int len_books(struct Book* ptr);
 int compare_str(char* str1, char* str2);
@@ -43,13 +42,12 @@ int main(){
         copy_str(book_list[i].publisher, publisher[i]);
         book_list[i].borrowed = borrow_possilble[i];
     }
-    int cuurnet_count = 10; // 구체는 배열처럼 '\0'가 없어서 따로 변수를 둬야함
+    int current_count = 10; // 구체는 배열처럼 '\0'가 없어서 따로 변수를 둬야함
+    int* pcount = &current_count;
     int sturct_size = sizeof(book_list[0]);
     struct Book* ptr = book_list;
 
-    find(ptr, cuurnet_count, sturct_size);
-    
-/*
+   
     puts("======== 도서관에 오신 것을 환영합니다 ========");
     while(1){
         puts("[1] : 도서 검색\t[2] : 도서 추가\t[3] : 도서 대출\t[4] : 도서 반납\t[5] : 책 리스트\t[6] : 종료");
@@ -57,18 +55,18 @@ int main(){
         int input = safety_input_num();
 
         switch(input){
-            case 1: find(ptitle, pauthor, ppublisher, pborrow_possilble); break;
-            case 2 : add_book(ptitle, pauthor, ppublisher, &len_book, pttmp, patmp, pptmp, pborrow_possilble); break;
-            case 3 : borrow_book(ptitle, pauthor, ppublisher, pborrow_possilble); break;
-            case 4 : return_book(ptitle, pauthor, ppublisher, pborrow_possilble); break;
-            case 5 : view_books(ptitle, pauthor, ppublisher, pborrow_possilble);
+            case 1: find(ptr, current_count, sturct_size); break;
+            case 2 : add_book(ptr, pcount); break;
+            case 3 : borrow_book(ptr, pcount, sturct_size); break;
+            case 4 : return_book(ptr, pcount, sturct_size); break;
+            case 5 : view_books(ptr, current_count);
             default : break;
         }
         if(input == 6)
             break;
     }
  
-*/
+
     
    
     return 0;
@@ -98,62 +96,45 @@ void safety_input(char* p){
     return;
 }
 
-// void add_book(char** ptitle, char** pauthor, char** ppublisher, int* plen_books, 
-//     char (*pttmp)[MAX_OF_CONTENT], char (*patmp)[MAX_OF_CONTENT], char (*pptmp)[MAX_OF_CONTENT], int* pborrow_possible){
-//     if(ptitle == NULL || pauthor == NULL || ppublisher == NULL || plen_books == NULL || pborrow_possible == NULL
-//         || pttmp == NULL || patmp == NULL || pptmp == NULL || pborrow_possible == NULL){
-//         return;
-//     }
-//     if(*plen_books>=SIZE){
-//         return;
-//     }
-//     char* pt = pttmp[*plen_books];
-//     char* pa = patmp[*plen_books];
-//     char* pp = pptmp[*plen_books];
-
-//     printf("책 제목을 입력하세요 : ");
-//     safety_input(pt);
-
-//     printf("저자 이름을 입력하세요 : ");
-//     safety_input(pa);
-
-//     printf("출판사 이름을 입력하세요 : ");
-//     safety_input(pp);
-    
-    
-//     int len = (*plen_books);
-//     // (*ptitle)[len]= pttmp[len];
-//     // (*pauthor)[len] = patmp[len];
-//     // (*ppublisher[len] = pptmp[len];
-
-//     ptitle[len] = pttmp[len];
-//     pauthor[len] = patmp[len];
-//     ppublisher[len] = pptmp[len];
-//     pborrow_possible[len] = 1;
-
-//     (*plen_books)++;
-
-
-
-//     // memcpy(*ptitle + *plen_books, tmp_title, sizeof(char) * (strlen(tmp_title)+1));
-//     // memcpy(*pauthor + *plen_books, tmp_author, sizeof(char) * (strlen(tmp_author)+1));
-//     // memcpy(*ppublisher + *plen_books, tmp_publisher, sizeof(char) * (strlen(tmp_publisher)+1));
-//     //ptr가 NULL이므로 절대 하면 안됨
-
-//     return;
-
+void add_book(struct Book* ptr, int* pcount){
+    if(ptr == NULL || *pcount>=SIZE){
+        return;
+    }
     
 
-// }
+    char* pt = ptr[*pcount].name;
+    char* pa = ptr[*pcount].author;
+    char* pp = ptr[*pcount].publisher;
+
+    printf("책 제목을 입력하세요 : ");
+    safety_input(pt);
+
+    printf("저자 이름을 입력하세요 : ");
+    safety_input(pa);
+
+    printf("출판사 이름을 입력하세요 : ");
+    safety_input(pp);
+    
+    ptr[*pcount].borrowed = 1;
+
+    (*pcount)++;
+
+    // memcpy(*ptitle + *plen_books, tmp_title, sizeof(char) * (strlen(tmp_title)+1));
+    // memcpy(*pauthor + *plen_books, tmp_author, sizeof(char) * (strlen(tmp_author)+1));
+    // memcpy(*ppublisher + *plen_books, tmp_publisher, sizeof(char) * (strlen(tmp_publisher)+1));
+    //ptr가 NULL이므로 절대 하면 안됨
+
+    return;
+}
 
 
 void view_books(struct Book* ptr, int count){
 
     for(int i=0; i<count; i++){
         printf("[%d]\t", i+1);
-        printf("제목 : %s\t", ptr[i].name);
-        printf("저자 : %s\t", ptr[i].author);
-        printf("출판사 : %s\t", ptr[i].publisher);
+        printf("제목 : %-10s\t", ptr[i].name);
+        printf("저자 : %-10s\t", ptr[i].author);
+        printf("출판사 : %-10s\t", ptr[i].publisher);
         ptr[i].borrowed == 1 ? printf("대출 가능") : printf("대출 불가능");
         putchar('\n');
     }
@@ -175,7 +156,6 @@ void find(struct Book* ptr, int count, int struct_size){
    
     char tmp[30];
     char* p = tmp;
-    char* select;
 
     printf("[1] : 제목으로 검색\n[2] : 저자로 검색\n[3] : 출판사로 검색\n");
     do{
@@ -196,32 +176,33 @@ void find(struct Book* ptr, int count, int struct_size){
         return;
     do{
         if(1 == ch){
-            printf("책 제목을 입력하세요 : ");
-            select = ptr->name;
+            printf("책 제목을 입력하세요 : "); 
         }else if(2 == ch){
             printf("저자 이름을 입력하세요 : ");
-            select = ptr->author
         }else if(3 == ch){
             printf("출판사를 입력하세요 : ");
-            select = ptr->publisher
         }else{
             return;
         }
     
         safety_input(p);
 
-        if(!check_info(select, p, count, struct_size)){
+        if(!check_info(ptr, p, count, ch)){
             puts("검색 결과가 없습니다.");
         }
-    }while(!check_info(select,p,count,struct_size));
+    }while(!check_info(ptr, p,count, ch));
 
     puts("=============================================================================================================================");
     for(int i=0; i<count; i++){
-        if(compare_str(tmp, select + struct_size * i)){
+        char* target = NULL;
+        if(ch == 1) target = ptr[i].name;
+        else if(ch == 2) target = ptr[i].author;
+        else if(ch == 3) target = ptr[i].publisher;
+        if(compare_str(tmp, target)){
             printf("[%d]\t", i+1);
-            printf("제목 : %s\t", ptr[i].name);
-            printf("저자 : %s\t", ptr[i].author);
-            printf("출판사 : %s\t", ptr[i].publisher);
+            printf("제목 : %-10s\t", ptr[i].name);
+            printf("저자 : %-10s\t", ptr[i].author);
+            printf("출판사 : %-10s\t", ptr[i].publisher);
             ptr[i].borrowed == 1 ? printf("대출 가능\n") : printf("대출 불가능\n");
         }
     };
@@ -232,12 +213,16 @@ void find(struct Book* ptr, int count, int struct_size){
 
 }
 
-bool check_info(char* select, char* pinput, int size, int struct_size){
+bool check_info(struct Book* ptr, char* pinput, int size, int ch){
     int cnt = 0;
+    if (ptr == NULL) return false;
 
-    
     for(int i=0; i<size; i++){
-        if(compare_str(select + struct_size * i, pinput)){
+        char* target;
+        if(ch == 1) target = ptr[i].name;
+        else if(ch == 2) target = ptr[i].author;
+        else if(ch == 3) target = ptr[i].publisher;
+        if(compare_str(target, pinput)){
             cnt++;
         }
     }
@@ -286,40 +271,68 @@ int safety_input_num(){
     return num;
 }
 
-// void borrow_book(char** ptitle, char** pauthor, char** ppublisher, int* pborrow_possible){
-// }
+void borrow_book(struct Book* ptr, int* pcount, int struct_size){
+    if(ptr == NULL || *pcount<0 || pcount == NULL ){
+        return;
+    }
 
-
-// void return_book(char** ptitle, char** pauthor, char** ppublisher, int* pborrow_possible){
-//     if(ptitle == NULL || pauthor == NULL || ppublisher == NULL || pborrow_possible == NULL){
-//         return;
-//     }
-
-//     puts("===== 반납할 책 검색 =====");
-//     find(ptitle, pauthor, ppublisher, pborrow_possible);
-//     int idx;
+    puts("===== 대출할 책 검색 =====");
+    find(ptr, *pcount, struct_size);
+    int idx;
     
-//     do{
-//         printf("반납할 책의 번호를 입력하세요 : ");
-//         idx = safety_input_num() - 1;
+    do{
+        printf("대출할 책의 번호를 입력하세요 : ");
+        idx = safety_input_num() - 1;
 
-//         if(idx < 0 || idx >= len_books(ptitle)){
-//             printf("%d번 책의 정보가 없습니다.\n", idx + 1);
-//         }
-//     }while(idx < 0 || idx >= len_books(ptitle));
+        if(idx < 0 || idx >= *pcount){
+            printf("%d번 책의 정보가 없습니다.\n", idx + 1);
+        }
+    }while(idx < 0 || idx >= *pcount);
     
 
-//     printf("===== 반납 할 책의 정보 =====\n");
-//     printf("제 목 : %s\n", ptitle[idx]);
-//     printf("저 자 : %s\n", pauthor[idx]);
-//     printf("출판사 : %s\n", ppublisher[idx]);
-//     printf("==========================\n");
+    printf("===== 대출 할 책의 정보 =====\n");
+    printf("제 목 : %-10s\n", ptr[idx].name);
+    printf("저 자 : %-10s\n", ptr[idx].author);
+    printf("출판사 : %-10s\n", ptr[idx].publisher);
+    printf("==========================\n");
 
-//     printf("반납 하시겠습니까?\n[1] : 예\n[2] : 아니요\n입 력 : ");
-//     int ch2;
-//     ch2 = fgetc(stdin);
-//     while(getchar() != '\n');
-//     if('1'==ch2) pborrow_possible[idx] = 1;
+    printf("대출 하시겠습니까?\n[1] : 예\n[2] : 아니요\n입 력 : ");
+    int ch2;
+    ch2 = fgetc(stdin);
+    while(getchar() != '\n');
+    if('1'==ch2) ptr[idx].borrowed=0;
+
+}
+
+void return_book(struct Book* ptr, int* pcount, int struct_size){
+    if(ptr == NULL){
+        return;
+    }
+
+    puts("===== 반납할 책 검색 =====");
+    find(ptr, *pcount, struct_size);
+    int idx;
     
-//     return;
-// }
+    do{
+        printf("반납할 책의 번호를 입력하세요 : ");
+        idx = safety_input_num() - 1;
+
+        if(idx < 0 || idx >= *pcount){
+            printf("%d번 책의 정보가 없습니다.\n", idx + 1);
+        }
+    }while(idx < 0 || idx >= *pcount);
+    
+
+    printf("===== 반납 할 책의 정보 =====\n");
+    printf("제 목 : %-10s\n", ptr[idx].name);
+    printf("저 자 : %-10s\n", ptr[idx].author);
+    printf("출판사 : %-10s\n", ptr[idx].publisher);
+    printf("==========================\n");
+
+    printf("반납 하시겠습니까?\n[1] : 예\n[2] : 아니요\n입 력 : ");
+    int ch2;
+    ch2 = fgetc(stdin);
+    while(getchar() != '\n');
+    if('1'==ch2) ptr[idx].borrowed = 1;    
+    return;
+}
